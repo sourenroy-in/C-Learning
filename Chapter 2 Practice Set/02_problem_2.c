f0826d7822eac7e857f05da501b91516f3396b55
Souren Roy <sourenroy.in@gmail.com>
2026-01-02 19:07:45 +0530

    Add files via upload

---
+++ Chapter 2 Practice Set/02_problem_2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float a = 3.0/8;
+    // 0.375000
+    a = 3.0/8-2;
+    printf("%f",a);
+    return 0;
+}
---
+++ Chapter 2 Practice Set/03_problem_3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    // int a = 2342354;
+    int a = 3349895;
+    printf("The Value of a is %d",a%97);
+    return 0;
+}
