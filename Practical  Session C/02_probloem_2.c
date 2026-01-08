dca307dd6387114f7db9f7170ff9acfb6d51713e
souren roy <sourenroy.in@gmail.com>
2026-01-08 18:37:40 +0530

    C Code Practice

---
+++ Practical  Session C/02_probloem_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n1, n2;
+    
+    printf("Enter NUM1: ");
+    scanf("%d",&n1);
+    printf("Enter NUM2: ");
+    scanf("%d",&n2);
+
+    printf("The Addition of Two number is %d\n",n1+n2);
+    printf("The Subtraction of Two num is %d\n",n1-n2);
+    printf("The Multiplication of Two Number is %d\n",n1*n2);
+    printf("The Division of Two number is %d\n",n1/n2);
+    printf("The Reaminder of Two number is %d\n",n1%n2);
+    
+    return 0;
+}
