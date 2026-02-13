814c19a577071f8339dab561064a4de007df7f17
souren roy <sourenroy.in@gmail.com>
2026-02-13 20:12:31 +0530

    Angles

---
+++ Chapter 2 Practice Set/Radius.c
@@ -0,0 +1,12 @@
+//write a programe to calculate radius of a circel
+
+#include<stdio.h>
+int main(){ 
+    float radius ;
+    printf("enter radius ;");
+    scanf("%f", &radius);
+    //Value of pi = 3.14 and the formula is 3.14 * radius of circle * radius of circle 
+    printf("area is %f", 3.14 * radius * radius);
+
+return 0;
+    }
