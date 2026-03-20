209710f1ad8f93f1fe2f25cab928a5d1a7869e1d
sourenroy-in <sourenroy.in@gmail.com>
2026-03-20 20:11:57 +0530

    printf 01,02

---
+++ GfG/01_Problem1.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main() {
+    // C Program to Print "Hello World"
+    printf("Hello World");
+    
+    // C Program To Print Your Own Name
+    printf("Souren Roy");
+
+    return 0;
+}
+
