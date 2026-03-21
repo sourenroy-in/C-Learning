cc86fe2e25b308540d7bea2fcd24b781ac611cb5
sourenroy-in <sourenroy.in@gmail.com>
2026-03-21 21:15:15 +0530

    03, 04

---
+++ GfG/03_Problem3.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+
+int main() {
+    // Write a Program to Swap the values of two variables.
+    int a = 10;
+    int b = 20;
+    int c;
+
+    printf("Before Swapping values are a = %d, b = %d\n", a,b);
+
+    c = a, a = b; b = c;
+    
+    printf("After swapping Values are a = %d, b = %d\n",a,b );
+
+    return 0;
+
+}
+
---
+++ GfG/04_Problem4.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    // C Program to Find the Size of int, float, double, and char
+    printf("Size of int: %u bytes\n", sizeof(int));
+
+    printf("Size of float: %u bytes\n", sizeof(float));
+
+    printf("Size of double: %u bytes\n", sizeof(double));
+
+    printf("Size of char: %u bytes\n", sizeof(char));
+
+    return 0;
+}
