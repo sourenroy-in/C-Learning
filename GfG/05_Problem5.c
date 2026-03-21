0789a29092aa1abbf7bfcfc8044376e32e1582ee
sourenroy-in <sourenroy.in@gmail.com>
2026-03-21 21:20:59 +0530

    05

---
+++ GfG/05_Problem5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    //  Write a Program to find the Sum of two numbers entered by the user.
+
+    int n1,n2;
+    
+        printf("Enter The Number 1: ");
+        scanf("%d",&n1);
+    
+        printf("\nEnter The Number 2: ");
+        scanf("%d",&n2);
+
+        printf("Sum of the Two User Defined Number is: %d", n1 + n2);
+
+        return 0;
+    
+}
