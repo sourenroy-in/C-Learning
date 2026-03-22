90a855435f33fc54b06752135be7a86aaaafcb02
sourenroy-in <sourenroy.in@gmail.com>
2026-03-22 21:07:18 +0530

    Find the largest number among the three numbers.

---
+++ GfG/07_Problem7.c
@@ -0,0 +1,22 @@
+// Find the largest number among the three numbers.
+#include<stdio.h>
+
+int main() {
+    int num1, num2, num3;
+    printf("\nEnter Number1: ");
+    scanf("%d",&num1);
+    printf("\nEnter Number2: ");
+    scanf("%d",&num2);
+    printf("\nEnter Number3: ");
+    scanf("%d",&num3);
+
+    if(num1>=num2 && num1>=num3) {
+        printf("The greates Number is: %d",num1);
+    } else if (num2>=num1 && num2>=num3) {
+          printf("The greates Number is: %d",num2);
+    } else {
+        printf("The greates NUmber is %d",num3);
+    }
+
+    return 0;
+}
