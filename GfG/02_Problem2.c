a5cdba2cf1b16a59e8ab16eaa25c6fb927c6f243
sourenroy-in <sourenroy.in@gmail.com>
2026-04-08 20:53:52 +0530

    User Input

--- GfG/02_Problem2.c
+++ GfG/02_Problem2.c
@@ -2,8 +2,10 @@
 
 int main() {
     // Print an Integer Value in C
-    int x = 10;
-
+    int x = 0;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    
     printf("The integer no is %d", x);
     return 0;
 }
